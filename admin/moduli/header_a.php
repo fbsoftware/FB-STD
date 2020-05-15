b3e7387bad6632da9fadfa4a3d8e25fd235006ce
fbsoftware <fbsoftware@alice.it>
2020-05-15 19:45:31 +0200

    init

--- admin/moduli/header_a.php
+++ admin/moduli/header_a.php
@@ -1,20 +1,21 @@
+<link rel="stylesheet" type="text/css" href="css/styles-flexo.css" />
 <?php
 echo "<header>"; 
-echo "<div class='row'>";
+echo "<div class='flex row'>";
 
-echo "<div class='col-md-1'>";
+echo "<div class='item j-start'>";
 echo "<img class='marchio' src='images/logo/logo.png' alt='logo.png' title='logo' >";
 echo "</div>";
 
-echo "<div class='col-md-9'>";
+echo "<div class='item j-end'>";
 echo "<p>Amministrazione&nbsp;-&nbsp;".DB::$page_title."</p>"; 
 echo "</div>";
 
-echo "<div class='col-md-1'>";
+echo "<div class='item j-end'>";
 echo "<img class='img-utente' src='images/".$_COOKIE['admin'].".png' alt='".$_COOKIE['admin'].".png' title='".$_COOKIE['admin']."' >";
 echo "</div>";
 
-echo "<div class='col-md-1'>";
+echo "<div class='item j-end'>";
 echo "<p class='little'>Versione ".DB::$livello.".".DB::$rilascio.".".DB::$modify."</p>";
 echo "</div>";
 
