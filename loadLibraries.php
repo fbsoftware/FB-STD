c4d2e89fce04ea030f0233fe5f98c1d8dcba454a
fbsoftware <fbsoftware@alice.it>
2020-05-15 19:52:37 +0200

    flex

--- loadLibraries.php
+++ loadLibraries.php
@@ -2,7 +2,7 @@
 spl_autoload_register('my_autoloader');  
  
 function my_autoloader($className) {
-    $path = 'libFB_1_2_0/';
+    $path = 'libFB_2_0_0/';
     include $path.$className.'.php';
 }
 ?>
